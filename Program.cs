a8238241608a4dd497fe29e46b79527e26245ed0
L1lCH1LL <mr.kudinov-2004@mail.ru>
2023-01-20 03:01:28 +0300

    Готовый

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// // // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите два чила для сравнения: ");
+
+// int number1 = int.Parse(Console.ReadLine());
+// int number2 = int.Parse(Console.ReadLine());
+
+// if (number1 > number2)
+//     Console.WriteLine($"MAX = {number1} ");
+
+// else
+//     Console.WriteLine($"MAX = {number2} ");
+
+
+
+// // // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите два чила для сравнения: ");
+
+// int number1 = int.Parse(Console.ReadLine());
+// int number2 = int.Parse(Console.ReadLine());
+// int number3 = int.Parse(Console.ReadLine());
+
+// if (number1 > number2)
+//     if (number1 > number3)
+//     Console.WriteLine($"MAX = {number1} ");
+
+// if (number2 > number1)
+//     if (number2 > number3)
+//     Console.WriteLine($"MAX = {number2} ");
+
+// if (number3 > number1)
+//     if (number3 > number2)
+//     Console.WriteLine($"MAX = {number3} ");    
+
+
+
+// // // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//Console.WriteLine("Введите число: ");
+
+// int number1 = int.Parse(Console.ReadLine());
+
+// if (number1 % 2 == 0)
+//     Console.WriteLine("Число четное");
+
+// else
+//     Console.WriteLine("Число не четное");
+
+
+
+// // // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Введите число: ");
+// int i = 1;
+// int number2 = int.Parse(Console.ReadLine());
+// while (i <= number2)
+// {
+//     if (i % 2 == 0)
+//         Console.WriteLine(i);
+//     i += 1;
+// }
+
+
